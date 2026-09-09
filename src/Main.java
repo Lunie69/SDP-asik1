1ebd9ee324dcf18cfa4218205558a8d306f7a49d
Lunie_1 <6akhr1yar@gmail.com>
2026-09-09 22:02:14 +0500

    Third commit, + Director and 3 receipts

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
 package cheremsha;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        Cheremsha cheremsha = new CheremshaCooker()
-                .setWeight(125)
-                .setSause("KisloSladij")
-                .setFreshness("Svezhij")
-                .setSpicyLvl("Mild")
-                .setPrice(3000)
-                .build();
+        CheremshaDirector director = new CheremshaDirector();
 
-        System.out.println(cheremsha);
+        Cheremsha spicyCheremsha = director.buildSpicyCheremsha();
+        Cheremsha firmCheremsha = director.buildFirmenayaCheremsha();
+        Cheremsha cheapCheremsha = director.buildCheapCheremsha();
+
+        System.out.println("Spicy Cheremsha:");
+        System.out.println(spicyCheremsha);
+
+        System.out.println();
+
+        System.out.println("Firmenaya Cheremsha:");
+        System.out.println(firmCheremsha);
+
+        System.out.println();
+
+        System.out.println("Deshevaya Cheremsha:");
+        System.out.println(cheapCheremsha);
     }
 }
